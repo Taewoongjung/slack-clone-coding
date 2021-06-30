5d74f3e9f3b91250e87acb178ff88e09d05d3908
Taewoong <aipooh8882@naver.com>
2021-06-30 17:08:16 +0900

    inviting workspace

---
+++ front/components/inviteWorkspaceModal/index.tsx
@@ -0,0 +1,59 @@
+import React, {useCallback} from 'react';
+import Modal from "@components/Modal";
+import axios from "axios";
+import {useParams} from "react-router";
+import useInput from "@hooks/useInput";
+import useSWR from "swr";
+import {IChannel, IUser} from "@typings/db";
+import fetcher from "@utils/fetcher";
+import {Button, Input, Label} from "@pages/style";
+import {toast} from "react-toastify";
+
+interface Props {
+    show: boolean;
+    onCloseModal: () => void;
+    setShowInviteWorkspaceModal: (flag: boolean) => void;
+}
+const inviteWorkspaceModal = () => {
+    const { workspace } = useParams<{ workspace: string; channel: string}>();
+    const [ newMember, onChangeNewMemeber, setNewMember ] = useInput('');
+    const { data: userData } = useSWR<IUser>('/api/users', fetcher);
+    const { revalidate: revalidateChannel } = useSWR<IChannel[]>(
+      userData ? `/api/workspaces/${workspace}/channels` : null,
+        fetcher,
+    );
+
+    const onInviteMember = useCallback((e) => {
+        e.preventDefault();
+        if (!newMember || !newMember.trim()) {
+            return;
+        }
+        axios
+            .post(`/api/workspaces/${workspace}/members`, {
+                email: newMember,
+            })
+            .then(() => {
+                revalidateChannel();
+                setShowInviteWorkspaceModal(false);
+                setNewMember('');
+            })
+            .catch((error) => {
+                console.dir(error);
+                toast.error(error.response?.data, { position: 'bottom-center' });
+            });
+    }, [workspace]);
+
+    return (
+        <Modal show={show} onCloseModal={onCloseModal}>
+            <form onSubmit={onInviteMember}>
+                <Label id="member-label">
+                    <span>이메일</span>
+                    <Input id="member" type="email" value={newMember} onChange={onChangeNewMemeber} />
+                </Label>
+                <Button type={"submit"}>초대하기</Button>
+            </form>
+        </Modal>
+    );
+};
+
+export default inviteWorkspaceModal;
--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -106,6 +106,10 @@ const Workspace: VFC = () => {
         setShowCreateChannelModal(false);
     }, []);
 
+    const onClickInviteWorkspace = useCallback(() => {
+
+    }, []);
+
     if (!userData) {
         return <Redirect to="/login" />
     }
@@ -147,6 +151,8 @@ const Workspace: VFC = () => {
                     <MenuScroll>
                         <Menu show={showWorkspaceModal} onCloseModal={toggleWorkspaceModal} style={{ top: 95, left: 80 }}>
                             <WorkspaceModal>
+                                <h2>Sleact</h2>
+                                <button onClick={onClickInviteWorkspace}>워크스페이스에 사용자 초대</button>
                                 <button onClick={onClickAddChannel}>채널 만들기</button>
                                 <button onClick={onLogout}>로그아웃</button>
                             </WorkspaceModal>
@@ -179,6 +185,7 @@ const Workspace: VFC = () => {
                 onCloseModal={onCloseModal}
                 setShowCreateChannelModal={setShowCreateChannelModal}
             />
+            <InviteWorkspaceModal show={} onCloseModal={} setShowInviteWorkspaceModal={}></InviteWorkspaceModal>
         </div>
     );
 };
