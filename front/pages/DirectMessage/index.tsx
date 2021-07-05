2884d6e58421c757d7a4fa37850eb549025a13f1
Taewoong <aipooh8882@naver.com>
2021-07-05 23:27:54 +0900

    DM style added

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,9 +1,29 @@
 import Workspace from "@layouts/Workspace";
 import React from 'react';
+import gravatar from 'gravatar';
+import {useParams} from "react-router";
+import useSWR from "swr";
+import fetcher from "@utils/fetcher";
+import {Container, Header} from "@pages/DirectMessage/style";
 
 const DirectMessage = () => {
+    const { workspace, id } = useParams<{ workspace: string, id: string }>();
+    const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+    const { data: myData } = useSWR(`/api/users`, fetcher);
+
+    if (!userData || !myData) {
+        return null;
+    }
+
     return (
-        <div>dm</div>
+        <Container>
+            <Header>
+                <img src={gravatar.url(userData.email, { s:'24px', d: 'retro'})} alt={userData.nickname} />
+                <span>{userData.nickname}</span>
+            </Header>
+            {/*<ChatList />*/}
+            {/*<ChatBox />*/}
+        </Container>
     );
 };
 
