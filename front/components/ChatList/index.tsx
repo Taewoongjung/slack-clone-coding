5a640e18c94b25ad018c88a793270fce83283b93
Taewoong <aipooh8882@naver.com>
2021-07-05 23:49:20 +0900

    chat list added

---
+++ front/components/ChatList/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ChatZone, Section} from "@components/ChatList/style";
+
+const ChatList = () => {
+    return (
+        <ChatZone>
+            <Section>Secrtion</Section>
+        </ChatZone>
+    );
+};
+
+export default ChatList;
--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -5,6 +5,8 @@ import {useParams} from "react-router";
 import useSWR from "swr";
 import fetcher from "@utils/fetcher";
 import {Container, Header} from "@pages/DirectMessage/style";
+import ChatBox from "@components/ChatBox";
+import ChatList from "@components/ChatList";
 
 const DirectMessage = () => {
     const { workspace, id } = useParams<{ workspace: string, id: string }>();
@@ -21,8 +23,8 @@ const DirectMessage = () => {
                 <img src={gravatar.url(userData.email, { s:'24px', d: 'retro'})} alt={userData.nickname} />
                 <span>{userData.nickname}</span>
             </Header>
-            {/*<ChatList />*/}
-            {/*<ChatBox />*/}
+            <ChatList />
+            <ChatBox chat="" />
         </Container>
     );
 };
